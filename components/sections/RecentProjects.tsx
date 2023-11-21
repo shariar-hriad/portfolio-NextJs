aec22c63c17f1cd07143feffca29feb49c4ada6f
shariar-hriad <shariarhossainriad@gmail.com>
2023-11-21 18:26:50 +0600

    test

--- components/sections/RecentProjects.tsx
+++ components/sections/RecentProjects.tsx
@@ -1,16 +1,14 @@
-import { ProjectType } from '@/types'
-import ProjectCard from '../ProjectCard'
 import { SectionTitle } from '../shared'
 
-const fetchRecentProjects = async () => {
-    const response = await fetch(process.env.BASE_URL + '/project')
-    const data = await response.json()
+// const fetchRecentProjects = async () => {
+//     const response = await fetch(process.env.BASE_URL + '/project')
+//     const data = await response.json()
 
-    return data
-}
+//     return data
+// }
 
 const RecentProjects = async () => {
-    const projects = await fetchRecentProjects()
+    // const projects = await fetchRecentProjects()
 
     return (
         <section className='py-8 lg:py-16'>
@@ -18,11 +16,11 @@ const RecentProjects = async () => {
                 <SectionTitle title='Recent Projects' subtitle='Projects' />
 
                 {/* Latest Projects */}
-                <div className='mt-10 grid grid-cols-1 justify-center gap-5 md:grid-cols-2 lg:mt-20 lg:grid-cols-4'>
+                {/* <div className='mt-10 grid grid-cols-1 justify-center gap-5 md:grid-cols-2 lg:mt-20 lg:grid-cols-4'>
                     {projects.map((project: ProjectType) => (
                         <ProjectCard key={project._id} project={project} />
                     ))}
-                </div>
+                </div> */}
             </div>
         </section>
     )
